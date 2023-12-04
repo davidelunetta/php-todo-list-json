480372095e6201022384fcbbbfa16d4c7c44d136
davidelunetta <davidelunetta.27@gmail.com>
2023-12-04 16:42:35 +0100

    added axios by cdn

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  
     <title>PHP Todo</title>
 </head>
 <body>
@@ -21,6 +23,25 @@
     </ul>
   </div>
 
-  <script src="/js/script.js"></script>
+  
+  <?php
+// read_todos.php - Per leggere i Todo dal file JSON
+$file = '/model/todos.json';
+$data = file_get_contents($file);
+echo $data;
+?>
+
+<?php
+// write_todos.php - Per scrivere i Todo nel file JSON
+$file = '/model/todos.json';
+$postData = file_get_contents('php://input');
+$data = json_decode($postData, true);
+file_put_contents($file, json_encode($data));
+?>
+
+
+
+  <script src="./js/script.js"></script>
+
 </body>
 </html>
