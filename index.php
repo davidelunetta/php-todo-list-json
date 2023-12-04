cb7ef39f8f17f14897d8ba33ad6f9b3e58652de5
davidelunetta <davidelunetta.27@gmail.com>
2023-12-04 22:12:09 +0100

    modified btn

--- index.php
+++ index.php
@@ -5,22 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   
     <title>PHP Todo</title>
 </head>
 <body>
 
   <div id="app">
+    <div class="container ">
     <h1>Lista Todo</h1>
     <input type="text" v-model="newTodo">
-    <button @click="addTodo">Aggiungi</button>
+    <button class="btn btn-primary" @click="addTodo">Aggiungi</button>
+    <div>
+      <ul>
+        <li v-for="(todo, index) in todos" :key="index">
+          {{ todo }}
+          <button class="btn" @click="removeTodo(index)"><i class="fa-solid fa-xmark" style="color: #000000;">x</i></button>
+        </li>
+      </ul>
+    </div>
 
-    <ul>
-      <li v-for="(todo, index) in todos" :key="index">
-        {{ todo }}
-        <button @click="removeTodo(index)">Rimuovi</button>
-      </li>
-    </ul>
+    
+    </div>
+    
   </div>
 
   
